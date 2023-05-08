239f3fbba43b2d5776803e06ea5f9df093761d62
David Ward <spy.sappin.your.sentry@gmail.com>
2023-05-08 00:35:24 -0700

    Add favicons, update meta to generate based on post details (#1)
    * Add favicons, update meta to generate based on post details
    
    * Add function for updating meta image wil more failsafes
    
    * Make meta default image smaller, override svg to use default image
    
    * Enforce png on meta image format
    
    * Change regex to look for srcset instead of src
    
    * Completely rework regexes due to multiple urls being generated
    
    * Make full url for meta image

---
+++ src/siteconfig.ts
@@ -0,0 +1,16 @@
+interface SiteConfig {
+  author: string;
+  title: string;
+  description: string;
+  lang: string;
+  ogLocale: string;
+}
+
+export const siteConfig: SiteConfig = {
+  author: "David Ward",
+  title: "davidward.dev",
+  description:
+    "David Ward's personal site and blog dedicated to all things software testing, development, and more.",
+  lang: "en-US",
+  ogLocale: "en_US",
+};
