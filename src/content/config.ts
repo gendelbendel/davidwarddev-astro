2f97c771889b5524397e5920cc65ce2aeb620a15
David Ward <spy.sappin.your.sentry@gmail.com>
2023-05-07 23:23:32 -0700

    Add favicons, update meta to generate based on post details

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ const postsCollection = defineCollection({
     pubDate: z.date(),
     updateDate: z.date().optional(),
     description: z.string(),
-    author: z.string(),
+    author: z.string().optional(),
     image: z.object({
       url: z.string(),
       alt: z.string(),
